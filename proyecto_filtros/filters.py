beef3b1a33df87e3a624a43e51b8422f1210539e
Samuel <samubellon@gmail.com>
2024-11-09 12:58:51 +0100

    cambios

--- proyecto_filtros/filters.py
+++ proyecto_filtros/filters.py
@@ -1,9 +1,8 @@
 # Funciones relacionadas con el filtrado
 
 import numpy as np
-import scipy
 from scipy.io import wavfile
-from scipy.signal import butter
+from scipy.signal import butter, sosfilt
 
 import os
 
@@ -18,12 +17,11 @@ FR = 1000
 def cargarAudio(audio):
     # read(file) -> (rate: int, data: numpy array )
     signal = wavfile.read(audio)
-    print(signal[0])
 
     return signal
 
 
-def calcularFrecuenciasCentrales(b, x): 
+def calcMidFrecuencies(b, x): 
     # (b, x) -> fm
 
     fm = FR * (G ** (x/b))  # Exact mid-band frecuencies [array]
@@ -32,13 +30,24 @@ def calcularFrecuenciasCentrales(b, x):
 
 def thirdOctaveFilter():
 
-    signal = cargarAudio(PINK_NOISE)
+    fs, signal_data = cargarAudio(PINK_NOISE)
     b = 3
     x = np.arange(-16, 14)  # No incluye el último valor
-    fm = calcularFrecuenciasCentrales(b, x)  # Exact mid-band frecuencies [array]
-    
-    sos = butter(N, Wn, 'bandpass', False, 'sos', )
+    fm = calcMidFrecuencies(b, x)  # Exact mid-band frecuencies [array]
+    fl = fm * (G ** (-1/(2 * b)))
+    fh = fm * (G ** (1/(2 * b)))
 
+    N = 6   # Order of the filter
+    filtered_signals = np.array([])
 
+    for f_low, f_High in zip(fl, fh):
+
+        sos = butter(N, [f_low, f_High], 'bandpass', False, 'sos', fs)
+        filtered_signals =np.append(filtered_signals, sosfilt(sos, signal_data))
+
+    # print(sos)
+
+    print(filtered_signals)
 
 cargarAudio(PINK_NOISE)
+thirdOctaveFilter()
