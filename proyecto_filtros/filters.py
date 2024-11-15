8b9a8bdd542c6b7570ca2c68a009962028a0714c
Samuel <samubellon@gmail.com>
2024-11-15 13:33:25 +0100

    calculo de RMS, SPL(dB) y graficado

--- proyecto_filtros/filters.py
+++ proyecto_filtros/filters.py
@@ -12,6 +12,10 @@ import os
 
 
 PINK_NOISE = os.path.join("data", "pink_noise_mono_48khz_16bits.wav")
+WHITE_NOISE = os.path.join("data", "white_noise_mono_48khz_16bits.wav")
+NOMINAL_THIRDOCTAVE_FREC = [25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400,
+                             500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000,
+                               6300, 8000, 10000, 12500, 16000, 20000]
 G =  10 ** (3/10)   # Octave frecuency ratio
 FR = 1000
 
@@ -31,9 +35,22 @@ def calcMidFrecuencies(b, x):
     return fm
 
 
-def thirdOctaveFilter():
+def calcRMS(filtered_values):
 
-    fs, signal_data = cargarAudio(PINK_NOISE)
+    rms_values = []
+
+    for filtered in filtered_values:
+        rms = np.sqrt(np.mean(filtered**2))
+        rms_values.append(rms)
+
+    # rms_values = [np.sqrt(np.mean(filtered ** 2)) for filtered in filtered_values]
+
+    return rms_values
+
+
+def thirdOctaveFilter(audio):
+
+    fs, signal_data = cargarAudio(audio)
     b = 3
     x = np.arange(-16, 14)  # No incluye el último valor
     fm = calcMidFrecuencies(b, x)  # Exact mid-band frecuencies [array]
@@ -41,17 +58,37 @@ def thirdOctaveFilter():
     fh = fm * (G ** (1/(2 * b)))
 
     N = 6   # Order of the filter
-    filtered_signals = np.array([])
+    band_levels = []   # Array con las bandas filtradas
 
     for f_low, f_High in zip(fl, fh):
+        sos = butter(N, [f_low, f_High], 'bandpass', False, 'sos', fs)  # Second Order Sections
 
-        sos = butter(N, [f_low, f_High], 'bandpass', False, 'sos', fs)
-        filtered_signals =np.append(filtered_signals, sosfilt(sos, signal_data))
+        filtered_signal = sosfilt(sos, signal_data)
 
-    # print(sos)
+        rms = np.sqrt(np.mean(filtered_signal**2))
+        level = 20 * np.log10(rms)
 
-    # print(filtered_signals)
+        band_levels.append(level)
 
 
-cargarAudio(PINK_NOISE)
-thirdOctaveFilter()
+    # Graficar niveles por bandas con barras uniformes
+    widths = np.array(fh) - np.array(fl)    # Calcular ancho de las barras en escala logarítmica
+    plt.figure(figsize=(10, 6))
+    plt.bar(fm, band_levels, width=widths, align='center', color='skyblue', edgecolor='black')
+    plt.xscale('log')
+    plt.xlabel('Frecuencia (Hz)')
+    plt.ylabel('Nivel (dB)')
+    plt.title('Niveles por bandas de tercios de octava (sin normalizar)')
+
+    # Personalizar el eje X para mostrar todas las frecuencias centrales
+    plt.xticks(fm, labels=[f"{int(freq)} Hz" if freq >= 100 else f"{freq:.1f} Hz" for freq in NOMINAL_THIRDOCTAVE_FREC], rotation=90)
+
+    # Agregar la cuadrícula
+    plt.grid(True, which="both", linestyle='--', linewidth=0.5)
+    plt.tight_layout()
+    plt.show()
+
+
+thirdOctaveFilter(PINK_NOISE)
+thirdOctaveFilter(WHITE_NOISE)
+
